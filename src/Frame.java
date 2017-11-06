313f5985a7b14662e514686660d85bd717231eb8
Exorcist359 <Exorcist359@gmail.com>
2017-11-06 18:50:11 +0500

    Git correct

--- src/Frame.java
+++ src/Frame.java
@@ -1,9 +1,11 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.Wrapper;
 import java.util.Timer;
 import java.util.TimerTask;
 import javax.swing.*;
 
+import logic.FieldWrapper;
 import logic.GameController;
 import logic.SnakeBot;
 import logic.SnakeDirection;
@@ -11,6 +13,7 @@ import logic.SnakeDirection;
 
 public class Frame extends JFrame {
     private GameController gameController;
+    private FieldWrapper fieldWrapper;
     private int WIN_WIDTH = 500;
     private int WIN_HEIGHT = 500;
 
@@ -29,19 +32,19 @@ public class Frame extends JFrame {
             Graphics2D gr2d = (Graphics2D) g;
 
             gr2d.setPaint(Color.BLACK);
-            gameController.getWalls().forEach(wall ->
+            fieldWrapper.getWalls().forEach(wall ->
                     g.fillRect(wall.getPosition().x*SELL_SIZE, wall.getPosition().y*SELL_SIZE, SELL_SIZE, SELL_SIZE));
 
             gr2d.setPaint(Color.GREEN);
-            gameController.getSnakeParts().forEach(snakePart ->
+            fieldWrapper.getSnakeParts().forEach(snakePart ->
                     g.fillRect(snakePart.getPosition().x*SELL_SIZE, snakePart.getPosition().y*SELL_SIZE, SELL_SIZE, SELL_SIZE));
 
             gr2d.setPaint(Color.YELLOW);
-            gameController.getSnakeHeads().forEach(snakePart ->
+            fieldWrapper.getSnakeHeads().forEach(snakePart ->
                     g.fillRect(snakePart.getPosition().x*SELL_SIZE, snakePart.getPosition().y*SELL_SIZE, SELL_SIZE, SELL_SIZE));
 
             gr2d.setPaint(Color.RED);
-            gameController.getApples().forEach(snakePart ->
+            fieldWrapper.getApples().forEach(snakePart ->
                     g.fillRect(snakePart.getPosition().x*SELL_SIZE, snakePart.getPosition().y*SELL_SIZE, SELL_SIZE, SELL_SIZE));
 
         }
@@ -65,8 +68,9 @@ public class Frame extends JFrame {
 
     public void execute() throws Exception {
         gameController = new GameController();
-        WIN_HEIGHT = gameController.fieldHeight * SELL_SIZE+37;
-        WIN_WIDTH = gameController.fieldWidth * SELL_SIZE+14;
+        fieldWrapper = gameController.getFieldWrapper();
+        WIN_HEIGHT = fieldWrapper.getHeight() * SELL_SIZE+37;
+        WIN_WIDTH = fieldWrapper.getWidth() * SELL_SIZE+14;
 
         this.setBounds(100, 100, WIN_WIDTH, WIN_HEIGHT);
 
